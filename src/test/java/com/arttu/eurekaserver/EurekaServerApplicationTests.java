87ba47a6e2b209e11604b1bc84fa60430f39fad9
Artur Shaiekhov <arttu9994@gmail.com>
2021-05-26 21:21:05 +0300

    Initial eureka server commit

---
+++ src/test/java/com/arttu/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arttu.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
